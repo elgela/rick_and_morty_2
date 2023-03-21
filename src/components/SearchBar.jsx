939e9e11449ae220bba024efb54ee81a5ab26cac
elgela <gelatomarcelo@hotmail.comgit config --global user.name elgelagit config --global user.email gelatomarcelo@hotmail.com>
2023-03-21 03:10:36 -0300

    inc

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 export default function SearchBar(props) {
    return (
       <div>
-         <input type='search' />
+         <input type='search' class='bot' />
          <button onClick={props.onSearch}>Agregar</button>
       </div>
    );
